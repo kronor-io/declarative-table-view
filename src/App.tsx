767b2581886235ed2547e736b975a79b03e210ab
daquirm <leo.daquirm@gmail.com>
2025-06-26 19:55:28 +0200

    Move style imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,3 @@
-import 'primereact/resources/themes/lara-light-indigo/theme.css'; //theme
-import 'primereact/resources/primereact.min.css'; //core css
-import 'primeicons/primeicons.css'; //icons
-import 'primeflex/primeflex.css'; // flex
-
 import { useEffect, useRef, useState } from 'react';
 import { GraphQLClient } from 'graphql-request';
 import Table from './components/Table';
