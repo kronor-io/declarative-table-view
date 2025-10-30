4f49428dad2f0d850e6b63d1dde9c8e87d2b0aad
daquirm <leo.daquirm@gmail.com>
2025-10-30 11:28:32 +0100

    Add component library build

---
+++ src/lib/index.ts
@@ -0,0 +1,13 @@
+// Library entry point for consumers.
+// Import global styles so they are embedded via the JS bundle (using vite-plugin-css-injected-by-js).
+import '../index.css';
+
+export { default as App } from '../App';
+export type { AppProps } from '../App';
+
+// Re-export selected framework types that are useful to consumers.
+export type { View } from '../framework/view';
+export type { Runtime } from '../framework/runtime';
+export type { ColumnDefinition } from '../framework/column-definition';
+export type { FilterState } from '../framework/state';
+export type { FilterSchema } from '../framework/filters';
